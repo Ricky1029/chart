6a0c2b27351636d82d016fe6ddd1dcb96b93524c
Ricky1029 <ricardobeltran035@gmail.com>
2024-06-12 10:14:39 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import BarChart from './Charts/BarChart';
+import DoughnutChart from './Charts/DoughnutChart';
+import LineChart from './Charts/LineChart';
+import PieChart from './Charts/PieChart';
+
+function App() {
+  return (
+    <div className="App">
+      <PieChart />
+    </div>
+  );
+}
+
+export default App;
